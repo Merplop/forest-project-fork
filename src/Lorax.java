aec3929e5573f3f4cf71615ce6c050e1bf996dd1
Gavin McDiarmid <gmcdiarm@calpoly.edu>
2023-03-17 16:45:19 -0700

    Changed lorax to extend from fairy, added template method

--- src/Lorax.java
+++ src/Lorax.java
@@ -1,25 +1,38 @@
+import processing.core.PImage;
 
-public class Lorax extends Moveable implements Active, Animated{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+public class Lorax extends Fairy {
+
+    public static final String LORAX_KEY = "lorax";
+
+    public Lorax(String id, Point position, double actionPeriod, double animationPeriod, List<PImage> images) {
+        super(id, position, actionPeriod, animationPeriod, images);
+    }
 
     @Override
     public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
+        Optional<Entity> loraxTarget = world.findNearest(this.getPosition(), new ArrayList<>(List.of(DudeFull.class, DudeNotFull.class)));
 
+        if (loraxTarget.isPresent()) {
+            Point tgtPos = loraxTarget.get().getPosition();
+
+            // If Lorax is adjacent to a Dude
+            if (this.moveTo(world, loraxTarget.get(), scheduler)) {
+
+                // TODO Add functionality for talking to dude
+
+//                Sapling sapling = Sapling.createSapling(Sapling.SAPLING_KEY + "_" + loraxTarget.get().getId(), tgtPos, imageStore.getImageList(Sapling.SAPLING_KEY), 0);
+//
+//                world.addEntity(sapling);
+//                sapling.scheduleActions(scheduler, world, imageStore);
+            }
+        }
+
+        scheduler.scheduleEvent(this, ActivityAction.createActivityAction(this, world, imageStore), this.getActionPeriod());
     }
 
-    @Override
-    public double getAnimationPeriod() {
-        //TODO
-        return 0;
-    }
 
-    @Override
-    public void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore) {
-
-    }
-
-    @Override
-    public Point nextPosition(WorldModel world, Point destPos) {
-        return null;
-    }
 }
